773cd2e737db9de6fc4c094cba12c588f064b23f
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-08-02 09:03:44 +0530

    Roman to Integer

---
+++ easy/math/roman-to-integer/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> values = new HashMap<>();
+        values.put('I', 1);
+        values.put('V', 5);
+        values.put('X', 10);
+        values.put('L', 50);
+        values.put('C', 100);
+        values.put('D', 500);
+        values.put('M', 1000);
+
+        int tot = 0;
+        for (int i = 0; i < s.length(); i++){
+            int current = values.get(s.charAt(i));
+            if (i + 1 < s.length() && current < values.get(s.charAt(i + 1)) ){
+                tot -= current;
+            } else {
+                tot += current;
+            }
+        }
+        return tot;
+    }
+}
